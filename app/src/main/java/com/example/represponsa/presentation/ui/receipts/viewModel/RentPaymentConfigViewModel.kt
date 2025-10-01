df82bac7060b19384d24240ff50f60b28f1bed5d
vitoriacds <Vitoria.Souza-ext@ab-inbev.com>
2025-09-30 22:57:38 -0300

    implement receipts screen

--- app/src/main/java/com/example/represponsa/presentation/ui/receipts/viewModel/RentPaymentConfigViewModel.kt
+++ app/src/main/java/com/example/represponsa/presentation/ui/receipts/viewModel/RentPaymentConfigViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.represponsa.data.model.RentPaymentConfig
-import com.example.represponsa.data.repository.RepublicRepository
 import com.example.represponsa.domain.useCases.SaveRentPaymentConfigUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,10 +14,7 @@ import javax.inject.Inject
 @HiltViewModel
 class RentPaymentConfigViewModel @Inject constructor(
     private val saveConfigUseCase: SaveRentPaymentConfigUseCase,
-    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-
-    private val republicId: String = savedStateHandle["republicId"] ?: ""
 
     private val _uiState = MutableStateFlow(RentPaymentConfigUiState())
     val uiState: StateFlow<RentPaymentConfigUiState> = _uiState
