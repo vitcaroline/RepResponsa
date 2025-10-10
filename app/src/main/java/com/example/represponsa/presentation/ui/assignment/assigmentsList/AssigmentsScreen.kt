7b788c8e51a98a5051e8ab6262b917a7c97a5330
vitoriacds <Vitoria.Souza-ext@ab-inbev.com>
2025-10-10 20:51:45 -0300

    validate user role to enable assigments edit

--- app/src/main/java/com/example/represponsa/presentation/ui/assignment/assigmentsList/AssigmentsScreen.kt
+++ app/src/main/java/com/example/represponsa/presentation/ui/assignment/assigmentsList/AssigmentsScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
